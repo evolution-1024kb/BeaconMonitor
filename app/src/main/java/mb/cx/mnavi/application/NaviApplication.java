1578fc56ca0db73440dae156ebaf9927a5838ba8
Toshiaki Ito <evolution@1024kb.cx>
2017-01-22 06:00:36 +0900

    add realm implements.

---
+++ app/src/main/java/mb/cx/mnavi/application/NaviApplication.java
@@ -0,0 +1,28 @@
+package mb.cx.mnavi.application;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
+
+/**
+ * アプリケーションクラス
+ * Created by toshiaki on 2017/01/22.
+ */
+
+public class NaviApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
+    }
+}
