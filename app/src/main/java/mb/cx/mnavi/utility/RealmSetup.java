01d96103d182e3a284410c8b8d25f10b24d3d12f
Toshiaki Ito <evolution@1024kb.cx>
2017-01-22 19:19:05 +0900

    add test data.
    refs #15

--- app/src/main/java/mb/cx/mnavi/utility/RealmSetup.java
+++ app/src/main/java/mb/cx/mnavi/utility/RealmSetup.java
@@ -24,8 +24,37 @@ public class RealmSetup {
                 one.setUuid("UUID");
                 one.setMajor(1);
                 one.setMinor(1);
+
+                final Item two = realm.createObject(Item.class, 2L);
+                two.setTitle("TWO");
+                two.setBody("TWO-TWO");
+                two.setUuid("UUID");
+                two.setMajor(1);
+                two.setMinor(2);
+
+                final Item thr = realm.createObject(Item.class, 3L);
+                thr.setTitle("THREE");
+                thr.setBody("THREE-THREE");
+                thr.setUuid("UUID");
+                thr.setMajor(1);
+                thr.setMinor(3);
+
+                final Item fur = realm.createObject(Item.class, 4L);
+                fur.setTitle("FOUR");
+                fur.setBody("FOUR-FOUR");
+                fur.setUuid("UUID");
+                fur.setMajor(1);
+                fur.setMinor(4);
+
+                final Item fiv = realm.createObject(Item.class, 5L);
+                fiv.setTitle("FIVE");
+                fiv.setBody("FIVE-FIVE");
+                fiv.setUuid("UUID");
+                fiv.setMajor(1);
+                fiv.setMinor(5);
             }
         });
+        realm.close();
     }
 
 }
