e3e5c4de399d46f917086ac1dc45696a00014899
Toshiaki Ito <evolution@1024kb.cx>
2017-01-22 18:44:26 +0900

    add realm object classes.
    refs #15

---
+++ app/src/main/java/mb/cx/mnavi/realm/Item.java
@@ -0,0 +1,10 @@
+package mb.cx.mnavi.realm;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by toshiaki on 2017/01/22.
+ */
+
+public class Item extends RealmObject {
+}
---
+++ app/src/main/java/mb/cx/mnavi/utility/RealmSetup.java
@@ -0,0 +1,8 @@
+package mb.cx.mnavi.utility;
+
+/**
+ * Created by toshiaki on 2017/01/22.
+ */
+
+public class RealmSetup {
+}
