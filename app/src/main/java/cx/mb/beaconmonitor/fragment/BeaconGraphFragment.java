455f296b5ea2982e6c2c85467162d6739dfccf87
Toshiaki Ito <evolution@1024kb.cx>
2017-04-29 12:33:16 +0900

    create sample chart.
    refs #25

--- app/src/main/java/cx/mb/beaconmonitor/fragment/BeaconGraphFragment.java
+++ app/src/main/java/cx/mb/beaconmonitor/fragment/BeaconGraphFragment.java
@@ -6,18 +6,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cx.mb.beaconmonitor.R;
 import cx.mb.beaconmonitor.event.BeaconSelectEvent;
+import cx.mb.beaconmonitor.model.YourData;
 import trikita.log.Log;
 
 /**
  * Graph fragment.
  */
 public class BeaconGraphFragment extends Fragment {
+    /**
+     * ButterKnife un binder.
+     */
+    private Unbinder unBinder;
+
+    /**
+     * Line chart.
+     */
+    @BindView(R.id.beacon_graph_chart)
+    LineChart chart;
 
     /**
      * Selected uuid.
@@ -42,7 +64,18 @@ public class BeaconGraphFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beacon_graph, container, false);
+        final View view = inflater.inflate(R.layout.fragment_beacon_graph, container, false);
+        unBinder = ButterKnife.bind(this, view);
+
+        createSampleChart();
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unBinder.unbind();
     }
 
     @Override
@@ -61,15 +94,44 @@ public class BeaconGraphFragment extends Fragment {
 
     /**
      * Receive beacon selected event.
+     *
      * @param event event object.
      */
     @SuppressWarnings("unused")
     @Subscribe(threadMode = ThreadMode.MAIN)
-    public void handleBeaconSelect(BeaconSelectEvent event){
+    public void handleBeaconSelect(BeaconSelectEvent event) {
         Log.d("UUID:%s, MAJOR:%d, MINOR:%d", event.getUuid(), event.getMajor(), event.getMinor());
 
         uuid = event.getUuid();
         major = event.getMajor();
         minor = event.getMinor();
+    }
+
+    private void createSampleChart() {
+
+        YourData[] dataObjects = new YourData[10];
+        dataObjects[0] = new YourData(1, 1);
+        dataObjects[1] = new YourData(2, 8);
+        dataObjects[2] = new YourData(3, 2.6f);
+        dataObjects[3] = new YourData(4, 4);
+        dataObjects[4] = new YourData(5, 9);
+        dataObjects[5] = new YourData(6, 10);
+        dataObjects[6] = new YourData(7, -4);
+        dataObjects[7] = new YourData(8, 0);
+        dataObjects[8] = new YourData(9, 9);
+        dataObjects[9] = new YourData(10, 10);
+
+        List<Entry> entries = new ArrayList<Entry>();
+
+        for (YourData data : dataObjects) {
+
+            // turn your data into Entry objects
+            entries.add(new Entry(data.getValueX(), data.getValueY()));
+        }
+
+        final LineDataSet dataSet = new LineDataSet(entries, "Label");
+        final LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate();
     }
 }
