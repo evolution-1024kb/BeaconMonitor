a5c49b61d3e6fcf55ec8ce982192f3e10f5cf71b
Toshiaki Ito <evolution@1024kb.cx>
2017-10-28 19:54:24 +0900

    remove import.
    refs #1

--- app/src/main/java/cx/mb/beaconmonitor/adapter/BeaconAdapter.java
+++ app/src/main/java/cx/mb/beaconmonitor/adapter/BeaconAdapter.java
@@ -1,7 +1,6 @@
 package cx.mb.beaconmonitor.adapter;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.TextView;
-
 import cx.mb.beaconmonitor.R;
 import cx.mb.beaconmonitor.realm.BeaconItem;
 import io.realm.OrderedRealmCollection;
